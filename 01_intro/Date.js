ca46be91484f58375c588c344931b2f8e5530596
Fahadali470 <f9384470@gmail.com>
2025-07-25 09:14:53 +0000

    Date

---
+++ 01_intro/Date.js
@@ -0,0 +1,17 @@
+const myDate= new Date();
+// console.log(myDate);
+// console.log(myDate.getDate());
+// console.log(myDate.toString());
+const nDate= new Date(2025,6,25,1,47);
+// console.log(nDate.toLocaleString());
+// console.log(nDate);
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+let myTimeStamp= Date.now();
+console.log(Math.floor(myTimeStamp/1000));
+console.log(nDate.getTime()/1000)
+
