c92765185a38b827d484d1349517c0887b276d00
Fahadali470 <f9384470@gmail.com>
2025-07-25 08:20:52 +0000

    Math

---
+++ 01_intro/math.js
@@ -0,0 +1,24 @@
+// const score=100;
+// console.log(score);
+// const num= new Number(1000)
+// console.log(num.toExponential(2));
+// const numb=new Number(112.34567);
+// console.log(numb.toFixed(3));
+// console.log(numb.toPrecision(3));
+// console.log(num.toString());
+// console.log(num.toLocaleString(3));
+// <---------------------MATH-------------------->
+// console.log(Math);
+// console.log(Math.sqrt(39));
+// console.log(Math.random());
+// console.log(Math.abs(-4));
+// console.log(Math.ceil(5.9));
+// console.log(Math.round(5.9));
+// console.log(Math.floor(5.9));
+// console.log(Math.min(2,3,4,68,79,0));
+// console.log(Math.max(2,3,4,68,79,0));
+// console.log(Math.pow(6,2));
+console.log(Math.floor((Math.random())*6))
+
+
+
