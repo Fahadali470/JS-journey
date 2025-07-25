8ec82a1e16f810669bfa7f8734b2e36251dc8e14
Fahadali470 <f9384470@gmail.com>
2025-07-25 06:26:32 +0000

    operators

---
+++ 01_intro/operations.js
@@ -0,0 +1,18 @@
+// <----------------Operations----------------->
+let num1=3;
+let num2=10;
+let num3=num1+num2;
+console.log(num3);
+let str1="Hello ";
+let str2="Fahad Bhatti";
+console.log(str1+str2);
+
+let score=100;
+console.log(score++)
+let scre=100;
+console.log(++scre)
+console.log(1+"2");
+console.log("1"+2)
+console.log("1"+"2")
+console.log("1"+2+2)
+console.log(1+2+"3")
