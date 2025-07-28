753411a234c0f1f2918b09590ab44280170f28ba
Fahadali470 <f9384470@gmail.com>
2025-07-28 12:34:33 +0000

    datatypes

--- 01_intro/string_detail.js
+++ 01_intro/string_detail.js
@@ -6,7 +6,7 @@ console.log(`My name is ${str1.toLowerCase()} and my score is ${str2}`)
 console.log(str1.charAt(5))
 console.log(str1.indexOf('d'))
 
-const str= str1.substring(0,4);
+const str= str1.substring(0);
 console.log(str)
 //slice :-  it can also be done in -ve form
 const newstr=str1.slice(-9,-4)
